74b6bf7ac363944848b96c5e7a861cf4ed425d4e
Ferris Armstrong <fda@bu.edu>
2021-11-14 16:58:09 -0500

    update keyboard handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // standard
 use std::time::Duration;
+use std::collections::VecDeque;
 
 // sdl2
 use sdl2::event::Event;
@@ -9,74 +10,172 @@ use sdl2::render::{ WindowCanvas, Texture };
 use sdl2::rect::{ Point, Rect };
 use sdl2::image::{self, LoadTexture, InitFlag };
 
+const PLAYER_MOVEMENT_SPEED: i32 = 15;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+struct Player {
+    position: Point,
+    sprite: Rect,
+    speed: i32,
+    current_frame: i32,
+    direction: VecDeque<Direction>,
+}
+
+/// Returns the row of the spritesheet corresponding to the given direction
+fn direction_spritesheet_row(direction: Option<&Direction>) -> i32 {
+    use self::Direction::*;
+    match direction {
+        Some(Up) => 3,
+        Some(Down) => 0,
+        Some(Left) => 1,
+        Some(Right) => 2,
+        None => 0
+    }
+}
+
 fn render(
     canvas: &mut WindowCanvas,
     color: Color,
-    texture: &Texture, // Pass texture by reference
-    position: Point,
-    sprite: Rect,
+    texture: &Texture,
+    player: &Player,
 ) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
     let (width, height) = canvas.output_size()?;
 
+    let (frame_width, frame_height) = player.sprite.size();
+    let current_frame = Rect::new(
+        player.sprite.x() + frame_width as i32 * player.current_frame,
+        player.sprite.y() + frame_height as  i32 * direction_spritesheet_row(player.direction.back()),
+        frame_width,
+        frame_height,
+    );
+
     // Treat the center of the screen as the (0, 0) coordinate
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
-    canvas.copy(texture, sprite, screen_rect)?;
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, frame_width, frame_height);
+    canvas.copy(texture, current_frame, screen_rect)?;
 
     canvas.present();
 
     Ok(())
 }
 
+// Update player a fixed amount based on their speed.
+// WARNING: Calling this function too often or at a variable speed will cause the player's speed
+// to be unpredictable!
+fn update_player(player: &mut Player) {
+    use self::Direction::*;
+    match player.direction.back() {
+        Some(Left) => {
+            player.position = player.position.offset(-player.speed, 0);
+        },
+        Some(Right) => {
+            player.position = player.position.offset(player.speed, 0);
+        },
+        Some(Up) => {
+            player.position = player.position.offset(0, -player.speed);
+        },
+        Some(Down) => {
+            player.position = player.position.offset(0, player.speed);
+        },
+        None => {
+            // No direction, do nothing
+        },
+    }
 
+    // Only continue to animate if the player is moving
+    if player.speed != 0 {
+        // Cheat: using the fact that all animations are 3 frames (NOT extensible)
+        player.current_frame = (player.current_frame + 1) % 3;
+    }
+}
 
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
     // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
     // stay unused because if we don't have any variable at all then Rust will treat it as a
     // temporary value and drop it right away!
-    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG).unwrap();
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("Hack 'n' Slash", 800, 600)
-        .resizable()
+    let window = video_subsystem.window("game tutorial", 800, 600)
         .position_centered()
         .build()
-        .unwrap();
+        .expect("could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas().build().expect("could not make a canvas");
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make a canvas");
 
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("assets/bardo.png").unwrap();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-    let position = Point::new(0, 0);
-    // src position in the spritesheet
-    let sprite = Rect::new(0, 0, 26, 36);
+    let mut player = Player {
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+        speed: 0,
+        direction: VecDeque::new(),
+        current_frame: 0,
+    };
 
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    break 'running;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), repeat: false, .. } => {
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction.push_back(Direction::Left);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), repeat: false, .. } => {
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction.push_back(Direction::Right);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), repeat: false, .. } => {
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction.push_back(Direction::Up);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction.push_back(Direction::Down);
+                },
+                Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                    player.direction.pop_back();
+                    if player.direction.is_empty() {
+                        player.speed = 0;
+                    }
                 },
                 _ => {}
             }
         }
 
-        // Update Background Color
+        // Update
         i = (i + 1) % 255;
+        update_player(&mut player);
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite).unwrap();
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        // Time management!
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
-}
 
+    Ok(())
+}
